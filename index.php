909ad64722fabb3b965f6a936c656131a5f25d81
bzoit <71105100+bzoit@users.noreply.github.com>
2022-07-12 14:44:19 -0400

    new

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gulzar&display=swap" rel="stylesheet">
+    <title>Spotlight | Login</title>
+    <link rel="icon" href="img/logo-icon.jpg">
+</head>
+<body>
+    <div id="headerContainer">
+        <h1>Spotlight</h1>
+        <h2>Show the world your appreciation for the ones that mean the most to you.</h2>
+    </div>
+    <div id="login-container">
+        <form>
+            <span><?php echo $loginError; ?></span>
+            <br/>
+            <input class="input" name="email" type="email" placeholder="Email">
+            <br>
+            <input class="input" name="password" type="password" placeholder="Password">
+            <br>
+            <a id="forgot" href="/">Forgot Password?</a>
+            <br>
+            <input type="submit" class="button" placeholder="Login" value="Login">
+        </form>
+        <a href="/">
+            <button class="button">Create An Account</button>
+        </a>
+    </div>
+</body>
+</html>
